2fa248e548d0c2105e2bb44b2535761f343a19ac
William Kelley <williamkelley@Williams-MacBook-Pro.local>
2022-07-08 11:08:32 -0400

    excercise 4

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -5,7 +5,8 @@ import * as React from 'react'
 
 function Board() {
   // 🐨 squares is the state for this component. Add useState for squares
-  const squares = Array(9).fill(null)
+  const [squares, setSquares] = React.useState(Array(9).fill(null));
+  const nextValue = calculateNextValue(squares);
 
   // 🐨 We'll need the following bits of derived state:
   // - nextValue ('X' or 'O')
@@ -14,28 +15,38 @@ function Board() {
   // 💰 I've written the calculations for you! So you can use my utilities
   // below to create these variables
 
+
   // This is the function your square click handler will call. `square` should
   // be an index. So if they click the center square, this will be `4`.
   function selectSquare(square) {
     // 🐨 first, if there's already winner or there's already a value at the
     // given square index (like someone clicked a square that's already been
     // clicked), then return early so we don't make any state changes
+    if (winner || squares[square]) {
+      return;
+    };
+
     //
     // 🦉 It's typically a bad idea to mutate or directly change state in React.
     // Doing so can lead to subtle bugs that can easily slip into production.
     //
     // 🐨 make a copy of the squares array
     // 💰 `[...squares]` will do it!)
+    const squaresCopy = [...squares];
     //
     // 🐨 set the value of the square that was selected
     // 💰 `squaresCopy[square] = nextValue`
-    //
+    squaresCopy[square] = nextValue;
+
     // 🐨 set the squares to your copy
+    setSquares(squaresCopy);
   }
 
   function restart() {
     // 🐨 reset the squares
     // 💰 `Array(9).fill(null)` will do it!
+    setSquares(Array(9).fill(null));
+
   }
 
   function renderSquare(i) {
@@ -46,10 +57,15 @@ function Board() {
     )
   }
 
+  const winner = calculateWinner(squares);
+  const status = calculateStatus(winner, squares, nextValue);
+
   return (
     <div>
       {/* 🐨 put the status in the div below */}
-      <div className="status">STATUS</div>
+      <div className="status">
+        {status}
+        </div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
